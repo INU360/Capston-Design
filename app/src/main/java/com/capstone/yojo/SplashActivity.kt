7b108711591a603fe17e28839fe932a2af1b3bd1
Dong Hyun <nemo247300@gmail.com>
2023-02-28 17:26:59 +0900

    아파트 값 가져오기위해 코드 추가(일단 주석처리)

--- app/src/main/java/com/capstone/yojo/SplashActivity.kt
+++ app/src/main/java/com/capstone/yojo/SplashActivity.kt
@@ -21,8 +21,10 @@ class SplashActivity : AppCompatActivity() {
     //private val foodMap : HashMap<String, MutableList<PlaceData>> = hashMapOf()
 
     private val opiMarkerList : ArrayList<OpiMarkerData> = ArrayList()
+    private val aptMarkerList : ArrayList<OpiMarkerData> = ArrayList()
 
     private val detailsMap : HashMap<String, MutableList<DetailsData>> = hashMapOf()
+    private val detailsAptMap : HashMap<String, MutableList<DetailsData>> = hashMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +44,8 @@ class SplashActivity : AppCompatActivity() {
         val database = Firebase.database
 
         val opiReference = database.getReference("OpiMarker") // 오피스텔 카테고리 접근
+        //val aptReference = database.getReference("AptMarker")
+
         //val foodReference = database.getReference("FoodMarker")
         val placeReference = database.getReference("Marker")
 
@@ -384,7 +388,97 @@ class SplashActivity : AppCompatActivity() {
 
          */
 
+        /*  Firebase 아파트 값들 가져오기
 
+        aptReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // 최상위 경로의 자식들에 접근
+                for (ds in snapshot.children) {
+                    when (ds.key) {
+                        "Apt" -> {
+                            val opi = snapshot.child("Apt")
+                            // detailsList 의 값을 건물명으로 묶어 details (type, price, size, build) 저장함
+                            val mapKey = arrayListOf<String>()
+
+                            for (value in opi.children) {
+                                val apt = value.key
+                                mapKey.add(apt!!)
+
+                                val address = value.child("address").value as String?
+                                val build = value.child("build").value as Long?
+                                val dong = value.child("dong").value as String?
+                                val latitude = value.child("latitude").value as Double?
+                                val longitude = value.child("longitude").value as Double?
+                                val name = value.child("name").value as String?
+
+
+                                // 아파트 매물 details (type, price, size, build ) 담기위한 리스트
+                                val detailsList = mutableListOf<DetailsData>()
+
+                                // 02. 06 더 하위에 있는 값 가져오기
+
+                                var floor: Long?
+                                var price: Long?
+                                var size: Double?
+                                var type: String?
+                                var date : Long?
+                                var deposit : Long?
+                                var monthly : Long?
+
+                                val details = value.child("details")
+
+                                for (dt in details.children) {
+                                    floor = dt.child("floor").value as Long?
+                                    price = dt.child("price").value as Long?
+                                    size = dt.child("size").value as Double?
+                                    type = dt.child("type").value as String?
+                                    date = dt.child("date").value as Long?
+                                    deposit = dt.child("deposit").value as Long?
+                                    monthly = dt.child("monthly").value as Long?
+
+
+                                    // 02.07 : detailsAptList에 각 아파트의 details에 접근해 floor, price, size 저장함
+                                    detailsList.add(DetailsData(floor, price, size, type, date, deposit, monthly))
+                                    //Log.e("list ==" , detailsList.toString())
+                                }
+
+                                detailsAptMap[apt] = detailsList
+                                Log.e("detailsAptMap ", detailsAptMap.toString())
+
+
+                                val myset = OpiMarkerData(
+                                    name,
+                                    address,
+                                    apt,
+                                    dong,
+                                    build,
+                                    latitude,
+                                    longitude
+                                )
+
+                                aptMarkerList.add(myset)
+                            }
+
+                            Log.e("marker list ", aptMarkerList.toString())
+
+                        }
+
+                    }
+
+                }
+
+                saveAptMarkerList(aptMarkerList)
+                saveDetailsAptData(detailsAptMap)
+
+
+                launchMainActivity()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle the error
+            }
+        })
+        */
 
 
         opiReference.addValueEventListener(object : ValueEventListener {
@@ -556,6 +650,29 @@ class SplashActivity : AppCompatActivity() {
 
      */
 
+    /*
+    // 아파트 마커리스트 저장(aptMarkerList)
+    fun saveAptMarkerList(aptMarkerList: ArrayList<OpiMarkerData>) {
+        val prefs = getSharedPreferences("my_pref", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val gson = Gson()
+        val json = gson.toJson(aptMarkerList)
+        editor.putString("apt_marker_list", json)
+        editor.apply()
+    }
+     */
+
+    /*
+    // 저장된 아파트 마커리스트 가져오기
+    fun loadAptMarkerList(): ArrayList<OpiMarkerData> {
+        val prefs = getSharedPreferences("my_pref", Context.MODE_PRIVATE)
+        val json = prefs.getString("apt_marker_list", null)
+        val type = object : TypeToken<ArrayList<OpiMarkerData>>() {}.type
+        return Gson().fromJson(json, type)
+    }
+
+     */
+
     // 오피스텔 detailsMap 저장
     private fun saveDetailsData(detailsMap: HashMap<*, *>?) {
         val prefs = getSharedPreferences("my_pref", Context.MODE_PRIVATE)
@@ -568,7 +685,6 @@ class SplashActivity : AppCompatActivity() {
         } else {
             editor.putString("details_map", null)
         }
-
         editor.apply()
     }
 
@@ -582,6 +698,34 @@ class SplashActivity : AppCompatActivity() {
     }
 
      */
+
+    // 아파트 detailsAptMap 저장
+    private fun saveDetailsAptData(detailsAptMap: HashMap<*, *>?) {
+        val prefs = getSharedPreferences("my_pref", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+
+        if (detailsAptMap != null) {
+            val gson = Gson()
+            val json = gson.toJson(detailsAptMap)
+            editor.putString("details_apt_map", json)
+        } else {
+            editor.putString("details_apt_map", null)
+        }
+        editor.apply()
+    }
+
+    /*
+    // 아파트 detailsAptMap 가져오기
+    fun loadDetailsAptMap(): HashMap<String, MutableList<DetailsData>>? {
+        val prefs = getSharedPreferences("my_pref", Context.MODE_PRIVATE)
+        val json = prefs.getString("details_apt_map", null)
+        val type = object : TypeToken<HashMap<String, MutableList<DetailsData>>>() {}.type
+        return Gson().fromJson(json, type)
+    }
+
+     */
+
+
 
 
     private fun launchMainActivity() {
