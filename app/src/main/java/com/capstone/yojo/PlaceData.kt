021e9c8d7a1ccbc4abddfaea965bfe4f6d946a6f
Dong Hyun <nemo247300@gmail.com>
2023-03-27 23:19:49 +0900

    파이어베이스에서 주변 장소들 정보 가져옴

--- app/src/main/java/com/capstone/yojo/PlaceData.kt
+++ app/src/main/java/com/capstone/yojo/PlaceData.kt
@@ -1,5 +1,7 @@
 package com.capstone.yojo
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
 import java.io.Serializable
 
 data class PlaceData(
@@ -7,4 +9,17 @@ data class PlaceData(
     var address : String?,
     var latitude :Double?,
     var longitude :Double?
-) : Serializable
+) : Serializable, ClusterItem {
+
+    override fun getPosition(): LatLng {
+        return LatLng(latitude ?: 0.0, longitude ?: 0.0)
+    }
+
+    override fun getTitle(): String? {
+        return name
+    }
+
+    override fun getSnippet(): String? {
+        return address
+    }
+}
