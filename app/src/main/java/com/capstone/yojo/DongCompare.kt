cd6cbeef48123097d1187308bb402de9083b00e4
Dong Hyun <nemo247300@gmail.com>
2023-03-29 16:54:17 +0900

    팀장 (민영) 코드 수정

--- app/src/main/java/com/capstone/yojo/DongCompare.kt
+++ app/src/main/java/com/capstone/yojo/DongCompare.kt
@@ -9,6 +9,9 @@ class DongCompare : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /*동네추천 화면에서 추천받은 동네 받아오기*/
+        var result = intent.getStringExtra("result")
+
         /* 바인딩 선언 */
         val binding = DongCompareBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,17 +37,82 @@ class DongCompare : AppCompatActivity() {
             "연수동은 노인복지시설과 의료기관이 동 평균에 비해 많이 분포되어 있습니다. 청소년 유해시설이 3번째로 많은 동입니다.",
             "선학동은 모든 시설이 동 평균에 비해 적게 분포하고 있습니다. 청소년 유해 시설은 1개로 동평균(18곳)에 비해 적게 분포합니다.")
 
+        //초기화
         binding.songdo.setBackgroundResource(R.drawable.round_btn_blue2)
         binding.songdo.setTextColor(Color.WHITE)
         binding.content.setText(dongStr[0])
         binding.hashTag.setText(tagStr[0])
         binding.graph.setImageResource(img[0])
 
+        /*동네추천화면에서 값 넘어올 경우*/
+        if (result.equals("1")){
+            //동춘 보이기
+            binding.dongchun.setBackgroundResource(R.drawable.round_btn_blue2)
+            binding.dongchun.setTextColor(Color.WHITE)
+            binding.content.setText(dongStr[1])
+            binding.hashTag.setText(tagStr[1])
+            binding.graph.setImageResource(img[1])
+
+            //송도 안보이기
+            binding.songdo.setBackgroundResource(R.drawable.round_edge_btn_blue)
+            binding.songdo.setTextColor(resources.getColor(R.color.colorMain))
+        }
+        if (result.equals("2")){
+            //청학 보이기
+            binding.chunghak.setBackgroundResource(R.drawable.round_btn_blue2)
+            binding.chunghak.setTextColor(Color.WHITE)
+            binding.content.setText(dongStr[2])
+            binding.hashTag.setText(tagStr[2])
+            binding.graph.setImageResource(img[2])
+
+            //송도 안보이기
+            binding.songdo.setBackgroundResource(R.drawable.round_edge_btn_blue)
+            binding.songdo.setTextColor(resources.getColor(R.color.colorMain))
+        }
+        if (result.equals("3")){
+            //옥련 보이기
+            binding.okryeon.setBackgroundResource(R.drawable.round_btn_blue2)
+            binding.okryeon.setTextColor(Color.WHITE)
+            binding.content.setText(dongStr[3])
+            binding.hashTag.setText(tagStr[3])
+            binding.graph.setImageResource(img[3])
+
+            //송도 안보이기
+            binding.songdo.setBackgroundResource(R.drawable.round_edge_btn_blue)
+            binding.songdo.setTextColor(resources.getColor(R.color.colorMain))
+        }
+        if (result.equals("4")){
+            //청학 보이기
+            binding.yeonsoo.setBackgroundResource(R.drawable.round_btn_blue2)
+            binding.yeonsoo.setTextColor(Color.WHITE)
+            binding.content.setText(dongStr[4])
+            binding.hashTag.setText(tagStr[4])
+            binding.graph.setImageResource(img[4])
+
+            //송도 안보이기
+            binding.songdo.setBackgroundResource(R.drawable.round_edge_btn_blue)
+            binding.songdo.setTextColor(resources.getColor(R.color.colorMain))
+        }
+        if (result.equals("5")){
+            //청학 보이기
+            binding.sunhak.setBackgroundResource(R.drawable.round_btn_blue2)
+            binding.sunhak.setTextColor(Color.WHITE)
+            binding.content.setText(dongStr[5])
+            binding.hashTag.setText(tagStr[5])
+            binding.graph.setImageResource(img[5])
+
+            //송도 안보이기
+            binding.songdo.setBackgroundResource(R.drawable.round_edge_btn_blue)
+            binding.songdo.setTextColor(resources.getColor(R.color.colorMain))
+        }
+
+
+
         for(i in bindingDong.indices) {
             bindingDong[i].setOnClickListener {
                 for (j in bindingDong.indices){
                     bindingDong[j].setBackgroundResource(R.drawable.round_edge_btn_blue)
-                    bindingDong[j].setTextColor(Color.BLACK)
+                    bindingDong[j].setTextColor(resources.getColor(R.color.colorMain))
                 }
                 bindingDong[i].setBackgroundResource(R.drawable.round_btn_blue2)
                 bindingDong[i].setTextColor(Color.WHITE)
